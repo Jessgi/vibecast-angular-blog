5a90901b34bfa38d9a9b19aee8012dc83c0f16c7
Jessica Gizela Dev <devjessgizela@gmail.com>
2025-03-24 02:24:43 -0300

    atualiação softre

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { MenuTitleComponent } from "./components/menu-title/menu-title.component";
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angular-edm-blog';
---
+++ src/app/components/small-card/small-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { BigCardComponent } from '../big-card/big-card.component';
+import { MenuTitleComponent } from '../menu-title/menu-title.component';
+
+@Component({
+  selector: 'app-small-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './small-card.component.html',
+  styleUrls: ['./small-card.component.css']
+})
+export class SmallCardComponent {}
